b21e73b21caf1119f9ea606d0a5ea65aec8c41f0
Joan Andrés <andres6936@live.com>
2020-05-24 14:59:17 -0500

    Added test for verify format GIF89a

--- Test/TestReadBinary.cpp
+++ Test/TestReadBinary.cpp
@@ -45,4 +45,36 @@ int main()
 	{
 		std::cout << exception.what();
 	}
+
+	// Test Number Two, Verify the number magic of gif file
+	// Assumptions: The filename point to file of type .gif
+	// 		and it is a .gif file valid (aka: not corrupted).
+	try
+	{
+		std::vector dataBinary = Properties::Readable::readBinary("../Test/Images/Format.gif");
+
+		// Wikipedia say: A GIF file starts with an signature as GIF89a GIF87a
+		std::vector numberMagic(dataBinary.cbegin(), dataBinary.cbegin() + 6);
+
+		// The values in hexadecimal are: 47 49 46 38 39 61 for the signature GIF89a
+
+		// In binary 47 is 01000111
+		assert(numberMagic[0] == std::byte(0b01000111));
+		// In binary 49 is 01001001
+		assert(numberMagic[1] == std::byte(0b01001001));
+		// In binary 46 is 01000110
+		assert(numberMagic[2] == std::byte(0b01000110));
+		// In binary 38 is 00111000
+		assert(numberMagic[3] == std::byte(0b00111000));
+		// In binary 39 is 00111001
+		assert(numberMagic[4] == std::byte(0b00111001));
+		// In binary 61 is 01100001
+		assert(numberMagic[5] == std::byte(0b01100001));
+
+		std::cout << "Test Success, GIF89a Verify.\n";
+	}
+	catch (std::exception& exception)
+	{
+		std::cout << exception.what();
+	}
 }
